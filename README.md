9749c16252c567348aaa72120ecd9ea8719e6d2e
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-04-18 12:04:28 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Career Foundry Pokedex
+
+This is Achievement 1 for the Full Stack Immersion course from CareerFoundry.com.
+
+It'll be cool soon.
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let favoriteFood = "Oreos";
+let alertString = "I would like to eat " + Math.pow(2,16) + " " + favoriteFood;
+alert(alertString);
+
+document.write("Wow, that's a lot of " + favoriteFood + "!!")
