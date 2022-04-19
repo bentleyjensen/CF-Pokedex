a3d3fb44f62e35654ad591279533d0ca1135c203
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-04-19 07:18:17 -0700

    Print pokemon to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -30,4 +30,29 @@ pokemonList[4] = {
     types: ['ground']
 }
 
-document.write(JSON.stringify(pokemonList));
+// Iterate each pokemon
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`<h1>${pokemonList[i].name}</h1>`);
+
+    // print the height of the pokemon
+    // Leave a note if they're especially small or large
+    let height = pokemonList[i].height;
+    document.write(`<p>Height: ${height}`);
+    if (height > 5){
+        document.write(" (He's huge!)");
+    } else if (height <= 0.5) {
+        document.write(" (So cute and little!)");
+    }
+    document.write('</p>');
+
+
+    // Start types section
+    document.write(`<p>Types: `);
+    
+    // Iterate types for each pokemon
+    for (let j = 0; j < pokemonList[i].types.length; j++) {
+        document.write(`<br>${pokemonList[i].types[j]}`);
+    }
+    // End types section
+    document.write(`</p>`);
+}
