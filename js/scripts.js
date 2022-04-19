d00fabc21edfb6b9a980067e6aef633d544c34fe
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-04-19 05:29:58 -0700

    Add some pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
-let favoriteFood = "Oreos";
-let alertString = "I would like to eat " + Math.pow(2,16) + " " + favoriteFood;
-alert(alertString);
+let pokemonList = [];
 
-document.write("Wow, that's a lot of " + favoriteFood + "!!")
+pokemonList[0] = {
+    name: 'Gyarados',
+    height: 6.5,
+    types: ['water', 'flying']
+}
+
+pokemonList[1] = {
+    name: 'Ninetales',
+    height: 1.1,
+    types: ['fire']
+}
+
+pokemonList[2] = {
+    name: 'Houndour',
+    height: 0.6,
+    types: ['dark', 'fire']
+}
+
+pokemonList[3] = {
+    name: 'Mantine',
+    height: 2.1,
+    types: ['water', 'flying']
+}
+
+pokemonList[4] = {
+    name: 'Phanpy',
+    height: 0.5,
+    types: ['ground']
+}
+
+document.write(JSON.stringify(pokemonList));
