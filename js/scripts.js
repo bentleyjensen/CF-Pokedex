e4b0af9c1813bf6049de113ad8728690962e0298
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-04-21 22:28:54 -0700

    Update pokemon list so it's smaller

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,26 @@
-let pokemonList = [];
-
-pokemonList[0] = {
-    name: 'Gyarados',
-    height: 6.5,
-    types: ['water', 'flying']
-}
-
-pokemonList[1] = {
-    name: 'Ninetales',
-    height: 1.1,
-    types: ['fire']
-}
-
-pokemonList[2] = {
-    name: 'Houndour',
-    height: 0.6,
-    types: ['dark', 'fire']
-}
-
-pokemonList[3] = {
-    name: 'Mantine',
-    height: 2.1,
-    types: ['water', 'flying']
-}
-
-pokemonList[4] = {
-    name: 'Phanpy',
-    height: 0.5,
-    types: ['ground']
-}
+let pokemonList = [
+    {
+        name: 'Gyarados',
+        height: 6.5,
+        types: ['water', 'flying']
+    },{
+        name: 'Ninetales',
+        height: 1.1,
+        types: ['fire']
+    },{
+        name: 'Houndour',
+        height: 0.6,
+        types: ['dark', 'fire']
+    },{
+        name: 'Mantine',
+        height: 2.1,
+        types: ['water', 'flying']
+    },{
+        name: 'Phanpy',
+        height: 0.5,
+        types: ['ground']
+    }
+];
 
 // Iterate each pokemon
 for (let i = 0; i < pokemonList.length; i++) {
